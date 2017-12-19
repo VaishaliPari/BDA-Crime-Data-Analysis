d916b707dd1b1be001e54aada50e2681dab4dcb5
Akshay Tambe <apt321@nyu.edu>
2017-12-19 12:00:42 -0500

    Total Borough Yearly Brooklyn

---
+++ Plots/total_borough_yearly_BROOKLYN_plot.py
@@ -0,0 +1,31 @@
+#Brooklyn each year count
+#!/usr/bin/python
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import csv
+#define pie chart
+def make_autopct(values):
+    def my_autopct(pct):
+        total = sum(values)
+        val = int(round(pct*total/100.0))
+        return '{p:.2f}%\n({v:d})'.format(p=pct,v=val)
+    return my_autopct
+#read input file
+crimes = []
+borough = []
+with open('total_borough_yearly_BROOKLYN.out','r') as file:
+    plots = csv.reader(file, delimiter='\t')
+    for row in plots:
+        crimes.append(int(row[0]))
+        borough.append(row[1])
+#plot the pie chart
+fig, ax = plt.subplots(figsize=(15,15))
+ax.pie(crimes, labels=borough, autopct=make_autopct(crimes),
+        shadow=True, startangle=90)
+ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+plt.title('Crime Amount At Each Borough')
+plt.legend(loc=5)
+# plt.show()
+plt.savefig('total_borough_yearly_BROOKLYN.png')
